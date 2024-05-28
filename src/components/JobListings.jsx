6c7a2362c4c1ba49c5191b6d93a2b1b366a3a459
mommotti <matfen97@gmail.com>
2024-05-28 17:44:10 +0200

    edited

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,21 @@
+import jobs from '../jobs.json'
+import JobListing from './JobListing'
+const JobListings = ({ isHome = false}) => {
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  console.log(jobs)
+  return (
+        <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+          <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+          </h2>
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobListings.map((job)=>(
+              <JobListing key={job.id} job={ job } />           
+            ))}
+          </div>
+        </div>
+      </section>
+    )
+  }
+export default JobListings
