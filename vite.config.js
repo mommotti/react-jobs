61206659f95bea4138b74880a449cac03b9fd2a3
mommotti <matfen97@gmail.com>
2024-06-10 14:09:48 +0200

    edited proxy

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3030,
-    proxy: {
-      "/api": {
-        target: "https://react-jobs-backend.onrender.com/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://react-jobs-backend.onrender.com/",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
 });
