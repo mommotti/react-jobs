19884af48a3911d744d54106e788dd0124e7adac
mommotti <matfen97@gmail.com>
2024-06-10 14:14:17 +0200

    edited proxy

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -125,7 +125,7 @@ const JobPage = ({ deleteJob }) => {
 };
 
 const jobLoader = async ({ params }) => {
-  const res = await fetch(`/api/jobs/${params.id}`);
+  const res = await fetch(`/jobs/${params.id}`);
   const data = await res.json();
   return data;
 };
